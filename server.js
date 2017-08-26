e7f781a3086e95efddb268bf07d5cf66c275a539
Shirlon Banks <sssbanks@gmail.com>
2017-08-26 09:32:56 -0700

    heroku deploy debug

--- server.js
+++ server.js
@@ -5,24 +5,20 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-// Mongoose mpromise deprecated - use bluebird promises
-var Promise = require("bluebird");
 
-mongoose.Promise = Promise;
 
-var URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongo-scraper'; // Replace newsScraper with the name of your local mongodb database
+var URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongo-scraper'; 
 
 
 // Initialize Express
 var app = express();
 
-// Use morgan and body parser with our app
-app.use(logger("dev"));
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
